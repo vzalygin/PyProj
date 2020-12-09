701dfdd8c35c0a88b9045e61a1ad1f5013d226d5
Vyacheslav <via.zyn@ya.ru>
2020-12-09 19:36:45 +0300

    Теперь не падает при нулях

--- Equations.py
+++ Equations.py
@@ -28,6 +28,29 @@ def main():
     # print(a, b, c)
     a, b, c = [int(i) for i in input().split()]
     e = euclidean(a, b)
+    if a == 0 and b == 0:
+        if c == 0:
+            print('y = 1j')
+            print('x = 1k')
+        else:
+            print('No solution')
+        exit()
+    elif a == 0:
+        if c % b == 0:
+            c //= b
+            print('y = ' + str(c) + 'k')
+            print('x = 1j')
+        else:
+            print('No solution')
+        exit()
+    elif b == 0:
+        if c % a == 0:
+            c //= a
+            print('y = 1j')
+            print('x = ' + str(c) + 'k')
+        else:
+            print('No solution')
+        exit()
     if c % e != 0:
         print('No solution')
         exit()
