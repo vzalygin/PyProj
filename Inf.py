e844d26fe14a8d6002eeed3657b5df5e7d8f7dd0
Vyacheslav <via.zyn@ya.ru>
2020-12-10 14:30:43 +0300

    inf

--- Inf.py
+++ Inf.py
@@ -1,21 +1,7 @@
-def func(n, x, y):
-    if n == 1:
-        if (y-x) % 3 == 2:  # идём назад или идём вперёд через шаг
-            print(n, x, 6-y-x)
-            print(n, 6-y-x, y)
-        else:
-            print(n, x, y)
-    else:
-        func(n - 1, x, 6 - x - y)
-        if (y-x) % 3 != 2:
-            print(n, x, y)
-            func(n - 1, 6 - x - y, y)
-        else:
-            func(n-1, 6-x-y, y)
-            print(n, x, 6-x-y)
-            func(n-1, y, x)
-            print(n, 6-x-y, y)
-            func(n-1, x, y)
-
-
-func(int(input()), 1, 3)
+a = [int(x) for x in input().split()]
+x = int(input())
+for i in range(len(a)):
+    if a[i] < x:
+        print(i+1)
+        exit()
+print(len(a)+1)
